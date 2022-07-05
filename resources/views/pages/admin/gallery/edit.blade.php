1d0a93f9185abf3831b4c083978b340b060733e2
alaqsaka <aqsaabidin15@gmail.com>
2022-07-05 16:11:34 +0700

    feat: create edit page for gallery, editing edit and update function for gallery

--- resources/views/pages/admin/gallery/edit.blade.php
+++ resources/views/pages/admin/gallery/edit.blade.php
@@ -6,7 +6,7 @@
 
             <!-- Page Heading -->
             <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 class="h3 mb-0 text-gray-800">Edit Travel Package {{ $item->title }} </h1>
+                <h1 class="h3 mb-0 text-gray-800">Edit Gallery </h1>
             </div>
 
             @if ($errors->any())
@@ -21,48 +21,23 @@
 
             <div class="card shadow">
                 <div class="card-body">
-                    <form action="{{ route('gallery.update', $item->id) }}" method="POST">
+                    <form action="{{ route('gallery.update', $item->id) }}" method="POST" enctype="multipart/form-data">
                     @method('PUT')
                     @csrf
                         <div class="form-group">
-                            <label for="title">Title</label>
-                            <input type="text" class="form-control" name="title" id="title" value="{{ $item->title }}">
+                            <label for="travel_packages_id">Travel Package</label>
+                            <select name="travel_packages_id" required class="form-control" id="">
+                                <option value="{{ $item->travel_packages_id }}">Don't Change This</option>
+                                @foreach ($travel_packages as $travel_package)
+                                    <option value="{{ $travel_package->id }}">
+                                        {{ $travel_package->title }}
+                                    </option>
+                                @endforeach
+                            </select>
                         </div>
                         <div class="form-group">
-                            <label for="title">Location</label>
-                            <input type="text" class="form-control" name="location" id="location" value="{{ $item->location }}">
-                        </div>
-                        <div class="form-group">
-                            <label for="about">About This Trip</label>
-                            <textarea name="about" id="about" rows="3" class="form-control d-block w-100">{{ $item->about }}</textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="featured_event">Featured Event</label>
-                            <input type="text" class="form-control" name="featured_event" id="featured_event" value="{{ $item->featured_event }}">
-                        </div>
-                        <div class="form-group">
-                            <label for="language">Language</label>
-                            <input type="text" class="form-control" name="language" id="language" value="{{ $item->language }}">
-                        </div>
-                        <div class="form-group">
-                            <label for="foods">Foods</label>
-                            <input type="text" class="form-control" name="foods" id="foods" value="{{ $item->foods }}">
-                        </div>
-                        <div class="form-group">
-                            <label for="departure_date">Departure Date</label>
-                            <input type="date" class="form-control" name="departure_date" id="departure_date" value="{{ $item->departure_date }}">
-                        </div>
-                        <div class="form-group">
-                            <label for="duration">Duration</label>
-                            <input type="text" class="form-control" name="duration" id="duration" value="{{ $item->duration }}">
-                        </div>
-                        <div class="form-group">
-                            <label for="type">Type</label>
-                            <input type="text" class="form-control" name="type" id="type" value="{{ $item->type }}">
-                        </div>
-                        <div class="form-group">
-                            <label for="price">Price</label>
-                            <input type="number" class="form-control" name="price" id="price" value="{{ $item->price }}">
+                            <label for="image">Image</label>
+                            <input type="file" name="image" id="" placeholder="Image" class="form-control">
                         </div>
                         <button class="btn btn-primary btn-block">Update</button>
                     </form>
