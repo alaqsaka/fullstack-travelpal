af217e86f88b4dcb0e121caf47ad1f29107ef87f
alaqsaka <aqsaabidin15@gmail.com>
2022-07-13 21:54:11 +0700

    fix: columns layout, integrate be and fe

--- resources/views/pages/Explore/explore.blade.php
+++ resources/views/pages/Explore/explore.blade.php
@@ -22,29 +22,27 @@
             <h1 class="tes">Popular Trips</h1>
             <div class="row">
               @foreach ($items as $item)
-                <div class="col-md-4 col-lg-3 col-sm-6 mb-3">
-                  <div class="card" style="border-radius: 10px">
-                    <img src="{{ url("frontend/images/header-background@2x.jpg") }}" class="card-img-top travel-card" alt="">
+                <div class="col-md-6 col-lg-4 col-xl-3 col-sm-6 mb-3">
+                  <div class="card" style="border-radius: 10px; height: 450px; max-height: 450px;">
+                    <img src="{{ $item->galleries->count() ? Storage::url($item->galleries->first()->image) : '' }}" class="card-img-top travel-card" alt="" height="180" width="427">
                     <div class="card-body">
-                      <h6 class=""><strong>Explore Bali </strong></h6>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <h6 class=""><strong>{{ $item->title }}</strong></h6>
+                      <p style="font-weight: 300">{{ $item->location }}</p>
+                      <p class="card-text">{{ Str::limit($item->about, 100) }}</p>
                       <div class="row">
                         <div class="col">
-                          <p><i class="fas fa-map-marker-alt"></i>&nbsp;&nbsp;<strong>Bali</strong></p>
+                          <p><i class="fas fa-map-marker-alt"></i>&nbsp;&nbsp;<strong>{{ $item->type }}</strong></p>
                         </div>
                         <div class="col">
-                          <i class="fas fa-bed"></i>&nbsp;&nbsp;<strong>2 Nights</strong>
+                          <i class="fas fa-bed"></i>&nbsp;&nbsp;<strong>{{ $item->duration }}</strong>
                         </div>
                       </div>
-                      <a href="#" class="stretched-link"></a>
+                      <a href="{{ url('/detail', $item->slug) }}" class="stretched-link"></a>
                     </div>
                   </div>
                 </div>  
               @endforeach
-             
-              
-            </div> 
-            
+            </div>
           </div>
         </div>
       </main>
