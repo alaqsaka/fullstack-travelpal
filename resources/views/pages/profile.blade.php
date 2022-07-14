15ec0b518f7b132d7398175cf0dda44c47bfe6a2
alaqsaka <aqsaabidin15@gmail.com>
2022-07-14 17:47:35 +0700

    W.O.P profile

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -5,6 +5,12 @@
 @endsection
 
 @section('content')
-  
+  <div class="container">
+    <div class="d-flex d-md-inline-flex flex-column flex-md-row justify-content-center">
+        <img src="https://ui-avatars.com/api/?name={{ Auth::user()->name }}" width="60"  height="60px" class="rounded-circle" style="margin: 0 auto;">        
+        <h4 class="text-center mt-3" style="font-weight: 700;">Hello, {{ Auth::user()->name }}!</h4>
+    </div>
+    
+  </div>
   
 @endsection
